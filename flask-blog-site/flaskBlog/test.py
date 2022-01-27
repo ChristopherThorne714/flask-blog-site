d6b3e260a273c5e2d151749650c6ae01563ead1d
Sheikh Ahmed <67445725+SHEIKHIA@users.noreply.github.com>
2022-01-27 00:14:17 -0500

    added only register folder

---
+++ flask-blog-site/flaskBlog/test.py
@@ -0,0 +1,37 @@
+"""
+Pytest
+"""
+from app import app
+
+def test1():
+    """
+    This function tests that the flask application has a correct response code
+    when the application goes live
+    """
+    response = app.test_client().get("/info")
+    assert response.status_code == 200
+
+def test2():
+    """Dummy docstring"""
+    response = app.test_client().get("/login")
+    assert response.status_code == 200
+    
+def test3():
+    """Dummy docstring"""
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
+def test4():
+    """Dummy docstring"""
+    response = app.test_client().get("/account")
+    assert response.status_code == 302
+
+def test5():
+    """Dummy docstring"""
+    response = app.test_client().get("/register")
+    #test if the string is on the page
+    assert b"Email" in response.data
+    assert b"Username" in response.data
+    assert b"Password" in response.data
+    #assert b"Project" in response.data
+
